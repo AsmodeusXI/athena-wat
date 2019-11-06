2d3c319b040679ba175085cad28997e6668e54b0
Samuel Lawton <slawton@athenahealth.com>
2019-11-06 14:13:54 -0500

    feat: first commit react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const h1Style = {
+    color: "green",
+    marginBottom: 20
+  }; // JSX style objects (like CSS, but with Camel-Case in an object)
+
+  const users = [
+    { id: 1, name: "Sam", email: "s@l.com" },
+    { id: 2, name: "Akito", email: "a@f.com" },
+    { id: 3, name: "Hannah", email: "h@c.com" }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="header">App</h1>
+      <label htmlFor="testinput">First Name</label>
+      <input id="testinput"></input>
+      <p style={h1Style}>These are the basics.</p>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from 'react'; // if you omit a filepath, it looks in node_modules
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'; // COOL.
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, // JSX that's compiled down by Babel to React.createElement (which needs a single element)
+    document.getElementById('root'),
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const element = React.createElement(
+//     React.Fragment,
+//     null,
+//     React.createElement("h1", null, "Test"),
+//     React.createElement("p", null, "Another Test")
+// );
+
+// ReactDOM.render(
+//     element,    // You can do this without JSX but why...
+//                 // Thinking differently about "separation of concerns" in React.
+//                 // Separation of "user functionality" across multiple technologies (JS, HTML, CSS)
+//     document.getElementById('root'),
+// );
