d0489679677f91f76149b9d3584f2b9fe6be2121
Samuel Lawton <slawton@athenahealth.com>
2019-11-06 15:19:42 -0500

    feat: continuing the training

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  // STATE! If it renders, redraw the state!
+  const [users, setUsers] = useState([
+    { id: 1, name: "Sam", email: "s@l.com" },
+    { id: 2, name: "Akito", email: "a@f.com" },
+    { id: 3, name: "Hannah", email: "h@c.com" }
+  ]);
+
   const h1Style = {
     color: "green",
     marginBottom: 20
   }; // JSX style objects (like CSS, but with Camel-Case in an object)
 
-  const users = [
-    { id: 1, name: "Sam", email: "s@l.com" },
-    { id: 2, name: "Akito", email: "a@f.com" },
-    { id: 3, name: "Hannah", email: "h@c.com" }
-  ];
+  function handleDelete(id) {
+    // Update state so we know to re-render!
+    setUsers(users.filter(user => user.id !== id));
+    // State doesn't update YET; it's queued.
+    // setState batches state updates for performance reasons.
+  }
 
   return (
     <>
-      <h1 className="header">App</h1>
+      <h1 className="header">Users</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {/* Delay execution via arrow func; THIS EXPLAINS SO MUCH! */}
+            <button onClick={event => handleDelete(user.id)}>Delete!</button>
+            {user.name} is a good human being!
+          </li>
+        ))}
+      </ul>
       <label htmlFor="testinput">First Name</label>
       <input id="testinput"></input>
       <p style={h1Style}>These are the basics.</p>
